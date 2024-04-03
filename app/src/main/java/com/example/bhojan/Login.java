e9dd585fa975802bf978cf5babde05a641f6bfae
Suraj Giri <surajgiri974@gmail.com>
2024-04-03 21:18:35 +0530

    Bhojan

---
+++ app/src/main/java/com/example/bhojan/Login.java
@@ -0,0 +1,26 @@
+package com.example.bhojan;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowInsets;
+
+import com.example.bhojan.databinding.ActivityLoginBinding;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
